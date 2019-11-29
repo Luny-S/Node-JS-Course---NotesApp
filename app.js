cef8db9bd27e0231a1d11e1e4753101bea5a50cb
Aleksander <olek130@gmail.com>
2019-11-29 21:53:16 +0100

    Importing npm modules part done.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
-const utils = require('./utils.js');
-const notes = require('./notes.js');
+const validator = require('validator');
+const getNotes = require('./notes.js');
 
-console.log(notes());
+const msg = getNotes();
+console.log(msg);
 
-
+console.log(validator.isEmail("example.com"));
+console.log(validator.isURL("https://mead.io"))
