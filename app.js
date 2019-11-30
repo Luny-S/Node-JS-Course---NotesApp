9025b60e79042d1fa80fa0835ac82a3b051833ee
Aleksander <olek130@gmail.com>
2019-12-01 00:20:54 +0100

    Yargs part of the tutorial

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const validator = require('validator');
-const getNotes = require('./notes.js');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-const msg = getNotes();
-console.log(msg);
+yargs.version('1.1.0');
 
-console.log(validator.isEmail("example.com"));
-console.log(validator.isURL("https://mead.io"))
+//add,remove,read,list
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log("Adding a new note");
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log("Removing a note");
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log("Read a note");
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log("List notes");
+    }
+});
+
+console.log(yargs.argv);
