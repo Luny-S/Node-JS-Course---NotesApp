83d5d3ff08da783056aa90bdfc02b90e8017f6f9
Aleksander <olek130@gmail.com>
2019-12-01 22:04:31 +0100

    JSON parsing examples

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
+const fs = require('fs');
 
 yargs.version('1.1.0');
 
-//add,remove,read,list
 
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log("Adding a new note");
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note description',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
     }
 });
 
@@ -38,4 +51,4 @@ yargs.command({
     }
 });
 
-console.log(yargs.argv);
+yargs.parse();
